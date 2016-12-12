6e1102bbc7e9579590dd427e66e924c38911bd69
OlegKovalik <oleg.kovalik@gmail.com>
2016-12-13 01:50:04 +0200

    13122016

--- app/src/main/java/com/example/oleg/kovalik_homework2/MainActivity.java
+++ app/src/main/java/com/example/oleg/kovalik_homework2/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.oleg.kovalik_homework2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import static android.view.View.inflate;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +33,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void showData() {
-        TableLayout tableLayout = (TableLayout) findViewById(R.id.table_layuot);
 
-        for (int i = 0; i < 4; i++) {
-            TableRow tableRow = new TableRow(this);
-            for (int j = 0; j < 3; j++) {
-                View.inflate(this, R.layout.data_cell, tableRow);
-            }
-            tableLayout.addView(tableRow, new TableLayout.LayoutParams(
-                    TableLayout.LayoutParams.MATCH_PARENT,
-                    TableLayout.LayoutParams.MATCH_PARENT, 1.0f));
+        final GridLayout gridLayout = (GridLayout) findViewById(R.id.grid_layuot);
+
+        for (int i = 0; i < getResources().getInteger(R.integer.gridColumnCount) * getResources().getInteger(R.integer.gridRowCount); i++) {
+            View v = inflate(this, R.layout.data_cell, null);
+            GridLayout.LayoutParams params = (GridLayout.LayoutParams) new GridLayout.LayoutParams();
+            params.width = 200;
+            params.height = 200;
+            v.setLayoutParams(params);
+            gridLayout.addView(v);
         }
+
+        gridLayout.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View view, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
+                int width = (view.getWidth()) / getResources().getInteger(R.integer.gridColumnCount);
+                int height = (view.getHeight()) / getResources().getInteger(R.integer.gridRowCount);
+                int margin = (int) getResources().getDimension(R.dimen.dataButtonMargin);
+                for (int j = 0; j < getResources().getInteger(R.integer.gridColumnCount) * getResources().getInteger(R.integer.gridRowCount); j++) {
+                    gridLayout.getChildAt(j).layout(width * (j % getResources().getInteger(R.integer.gridColumnCount)) + margin, height * (j / getResources().getInteger(R.integer.gridColumnCount)) + margin,
+                            width * ((j % getResources().getInteger(R.integer.gridColumnCount) + 1)) - margin, height * ((j / getResources().getInteger(R.integer.gridColumnCount) + 1)) - margin);
+                }
+            }
+        });
     }
 
     @Override
@@ -66,7 +81,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
